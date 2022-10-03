8a871cacc4527fedb699613f547f4a6e2f24a8b8
Julien Rioux <julien.rioux@hotmail.com>
2022-10-03 13:03:01 -0400

    Adding google analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,10 @@ import { ReduceProviders } from 'hooks/reduce-providers';
 import { getProviders } from 'hooks/reduce-providers/providers';
 import { GlobalStyles } from 'styles/global-styles';
 import { AppRouter } from 'app-router';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-243959863-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
