b019ef8f04c2a97d0941e4689fdab7bc08100ee9
Julien Rioux <julien.rioux@hotmail.com>
2022-11-02 22:46:04 -0400

    Testing a 'fix...'

--- client/src/configs/index.ts
+++ client/src/configs/index.ts
@@ -1,4 +1,10 @@
-import { overrideHideAppFromUrlParams } from 'utils';
+const SEARCH_PARAM = 'override-hide-app';
+
+/** This function simply check if we should override witht he url params. */
+export const overrideHideAppFromUrlParams = () => {
+  const searchParams = new URLSearchParams(window?.location?.search);
+  return searchParams.get(SEARCH_PARAM) === 'true';
+};
 
 export const SHOW_PRODUCT_HUNT_BTN = false;
 export const APP_NAME = 'Chairleader';
