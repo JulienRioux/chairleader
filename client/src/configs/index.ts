e2fe710eee757104cff75246e58b38f1974bdf96
Julien Rioux <julien.rioux@hotmail.com>
2022-11-02 22:39:35 -0400

    WTF

--- client/src/configs/index.ts
+++ client/src/configs/index.ts
@@ -5,7 +5,8 @@ export const APP_NAME = 'Chairleader';
 export const USE_CATEGORY = false;
 export const APP_LOGO = '🪑';
 export const IS_POINT_OF_SALE = false;
-export const HIDE_APP = process.env.REACT_APP_HIDE_APP === 'true';
+export const HIDE_APP =
+  process.env.REACT_APP_HIDE_APP === 'true' && !overrideHideAppFromUrlParams();
 export const PAYMENT_SERVICE_FEE =
   (Number(process.env.REACT_APP_PAYMENT_SERVICE_FEE) ?? 0) * 100;
 export const SELLING_NFT_SERVICE_FEE =
