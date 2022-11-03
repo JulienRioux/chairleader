fd5f435842dc1f437274fe3c776a585e60518364
Julien Rioux <julien.rioux@hotmail.com>
2022-11-02 22:34:24 -0400

    Trying to resolve the issue

--- client/src/utils/override-hide-app-from-url-params/index.ts
+++ client/src/utils/override-hide-app-from-url-params/index.ts
@@ -2,6 +2,6 @@ const SEARCH_PARAM = 'override-hide-app';
 
 /** This function simply check if we should override witht he url params. */
 export const overrideHideAppFromUrlParams = () => {
-  const searchParams = new URLSearchParams(window.location.search);
+  const searchParams = new URLSearchParams(window?.location?.search);
   return searchParams.get(SEARCH_PARAM) === 'true';
 };
