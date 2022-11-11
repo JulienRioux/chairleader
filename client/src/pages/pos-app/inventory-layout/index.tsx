b0f27590fc07f7ef777ff7e02e729c86a4e13ce2
Julien Rioux <julien.rioux@hotmail.com>
2022-11-10 19:43:15 -0500

    Testing another fix 3.o

--- client/src/pages/pos-app/inventory-layout/index.tsx
+++ client/src/pages/pos-app/inventory-layout/index.tsx
@@ -76,6 +76,7 @@ const CartWrapper = styled.div<{ isClosing: boolean }>`
   right: 0;
   bottom: 0;
   -webkit-backface-visibility: hidden;
+  -webkit-overflow-scrolling: touch;
   overflow-y: scroll;
   border-left: 1px solid ${(p) => p.theme.color.lightGrey};
   width: 480px;
