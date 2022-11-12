46a692620e587538fafb7b83fbc4b0988b336f48
Julien Rioux <julien.rioux@hotmail.com>
2022-11-11 19:18:52 -0500

    Removing log

--- api/store/src/utils/connect-db/index.ts
+++ api/store/src/utils/connect-db/index.ts
@@ -4,8 +4,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log('process.env.DB_NAME', process.env.DB_NAME);
-
 // Setting up the MongoDB connection URI
 const MONGODB_CONNECTION_URI = `mongodb+srv://main-app:${process.env.MONGODB_PASSWORD}@cluster0.xrrliil.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
