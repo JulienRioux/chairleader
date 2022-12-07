02773b4128a38436a89025ee92274d0b1e1f2b1e
Julien Rioux <julien.rioux@hotmail.com>
2022-12-07 14:46:55 -0500

    FE-0 small changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,11 @@ import { getProviders } from 'hooks/reduce-providers/providers';
 import { GlobalStyles } from 'styles/global-styles';
 import { AppRouter } from 'app-router';
 import ReactGA from 'react-ga';
+import { welcomeLog } from 'welcome-log';
 
 ReactGA.initialize('UA-243959863-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
+welcomeLog();
 
 function App() {
   return (
